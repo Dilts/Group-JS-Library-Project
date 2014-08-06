1c0d80b4f0c19c625b1a7f770cf98a6b1dcc3000
Brian Dilts <brianedilts@gmail.com>
2014-08-06 16:07:36 -0600

    fixed size issues and added imageLoad

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
 $(document).on('ready', function() {
-  
+  	var container = document.querySelector('#container');
+	var msnry;
+// initialize Masonry after all images have loaded
+	imagesLoaded( container, function() {
+  		msnry = new Masonry( container, {
+ 		 columnWidth: 150,
+ 		 itemSelector: '.item'
+  		});
+
+
+
 });
+  		// options
+
+});
+
